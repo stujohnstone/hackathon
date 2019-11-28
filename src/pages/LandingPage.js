ace931a8b6f526e4d3772bf09ea0fa52b6a16b32
Stuart Johnstone <stu.johnstone@vistair.com>
2019-11-28 09:26:03 +0000

    wip

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,18 +1,47 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
 import NavMain from "../components/NavMain";
 import HeroUnit from "../components/HeroUnit";
+import ExamplePost from "../components/ExamplePost";
 import Map from "../components/Map";
 
-export const LandingPage = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    textAlign: 'center'
+  },
+});
+
+function LandingPage(props) {
+  const { classes } = props;
   return (
     <React.Fragment>
       <NavMain>
-        <HeroUnit />
-        <Map />
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <HeroUnit />
+            </Grid>
+            <Grid item xs={6}>
+                <ExamplePost></ExamplePost>
+            </Grid>
+            <Grid item xs={6}>
+                <Map />
+            </Grid>
+          </Grid>
+        </div>
       </NavMain>
     </React.Fragment>
   );
 };
 
-export default LandingPage;
+LandingPage.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(LandingPage);
