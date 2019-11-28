29eba35dd52af7b7abac3cb78401eff3bd91ec15
Stuart Johnstone <stu.johnstone@vistair.com>
2019-11-28 17:24:13 +0000

    wip

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.httpPort;
 
 app.use(express.static(publicPath));
 app.use(cors());
