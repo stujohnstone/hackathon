ec259ea0bfd6153e0d365613509caabf1c88ea19
Oli Drake <Oli.drake@vistair.com>
2019-11-29 13:24:21 +0000

    music fixed

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -2,17 +2,21 @@ import React, { Component } from "react"
 import {withStyles} from "@material-ui/core";
 
 class MusicPlayer extends React.Component {
-    playAudio() {
+    componentDidMount() {
+        console.log("mounted");
         const audioEl = document.getElementsByClassName("audio-element")[0]
         audioEl.play()
     }
+
+
+
 
     render() {
         console.log("render")
         return (
             <div>
                 <audio className="audio-element">
-                    <source src="https://vistairsysoncascadecom-my.sharepoint.com/personal/oli_drake_vistair_com/Documents/Frank%20Sinatra%20-%20Come%20fly%20with%20me.mp3"></source>
+                    <source src="/dist/resources/Frank Sinatra-Comeflywithme.mp3"></source>
                 </audio>
             </div>
         )
